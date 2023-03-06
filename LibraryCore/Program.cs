da85547a77d94abcd35a7d3bcdce79d91caa09f0
Jim Huang <jim.huang@asuscloud.com>
2023-03-06 16:21:19 +0800

    add : add date check and convert

--- LibraryCore/Program.cs
+++ LibraryCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryCore.Utility;
 using System;
+using WebStorageDrive.Utility;
 
 namespace LibraryCore
 {
@@ -11,6 +12,17 @@ namespace LibraryCore
             {
                 RunHelperFunction();
             }
+            else if (args[0].ToLower() == "/time")
+            {
+                string type = "0";
+                try
+                {
+                    type = args[1];
+                }
+                catch { }
+
+                GetNowSystemTime(type);
+            }
             else if (args[0].ToLower() == "/v") 
             {
                 GetSystemInfo();
@@ -20,8 +32,27 @@ namespace LibraryCore
             Console.Write("Press any key to continue...");
             Console.ReadKey();
         }
-
-
+        //
+        // 摘要:
+        //     /h：顯示幫助區塊，包括可使用指令。
+        private static void RunHelperFunction()
+        {
+            Console.WriteLine("使用方式：LibraryCore [/h] [/v]");
+            Console.WriteLine();
+            Console.WriteLine("/h-      查詢指令");
+            Console.WriteLine("/v-      查詢系統資訊");
+            Console.WriteLine("/time-   查詢目前時間 (搭配參數：0 為當地時間；1 為UTC 時間，預設為0)");
+        }
+        //
+        // 摘要:
+        //     /time [0/1]：取得當前時間，0 為當地時間；1 為 UTC 時間。
+        private static void GetNowSystemTime(string type)
+        {
+            Console.WriteLine(DateUtility.FormatADDataTimeFunction(DateTime.Now, string.IsNullOrEmpty(type) || type == "0").ToString());
+        }
+        //
+        // 摘要:
+        //     /v：取得系統資訊。
         private static void GetSystemInfo()
         {
             Console.WriteLine("App Name：" + ConfigUtility.Instance.GetExeutingName);
@@ -29,14 +60,6 @@ namespace LibraryCore
             Console.WriteLine("Manufacturer：" + ConfigUtility.Instance.SystemManufacturer);
             Console.WriteLine("Device Name：" + ConfigUtility.Instance.GetMachineName);
             Console.WriteLine("Device Model：" + ConfigUtility.Instance.SystemModel);
-        }
-
-        private static void RunHelperFunction()
-        {
-            Console.WriteLine("使用方式：LibraryCore [/h] [/v]");
-            Console.WriteLine();
-            Console.WriteLine("/h-      查詢指令");
-            Console.WriteLine("/v-      查詢系統資訊");
         }
     }
 }
